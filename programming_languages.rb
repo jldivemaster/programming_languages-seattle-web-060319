647ce5b08fa5c5fa151d789f13a834f87be63ce0
Jason Leach <jlspursfan@gmail.com>
2019-05-22 03:15:30 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 def reformat_languages(languages)
   new_hash = {}
-  
+
   languages.each do |style, lang_hash|
     arr = []
     lang_hash.each do |lang, attr_hash|
       if lang == :javascript
         attr_hash[:style] = languages.keys
         new_hash[lang] = attr_hash
-      else 
+      else
         arr << style
         attr_hash[:style] = arr.uniq
         new_hash[lang] = attr_hash
